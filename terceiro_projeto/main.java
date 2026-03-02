ed93b7fa80cfd5fa0762fedf9fe493340775401c
GabrieleLopes0 <gabrielelopesbibi@gmail.com>
2026-03-02 14:46:23 -0300

    Adiciona o terceiro projeto com exercícios de cálculo em Java

---
+++ terceiro_projeto/main.java
@@ -0,0 +1,95 @@
+package terceiro_projeto;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        /* 
+        //Exercicio 1
+        System.out.println("Iniciando programa para fazer a soma de dois numeros");
+        System.out.println("Digite o primeiro numero:");
+        double n1, n2, resultado;
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextDouble();
+        System.out.println("Digite o segundo numero:");
+        Scanner sc2 = new Scanner(System.in);
+        n2 = sc2.nextDouble();
+        resultado = n1 + n2;
+        System.out.println("O resultado da soma dos dois numeros é: " + resultado);*/
+
+        /* 
+        //Exercicio 2
+        System.out.println("Iniciando programa para calcular a area de um circulo com base no raio");
+        double raio, area, pi;
+        System.out.println("Digite o raio do circulo:");
+        Scanner sc = new Scanner(System.in);
+        raio = sc.nextDouble();
+        System.out.println("Para o calculo PI será considerado 3.14159");
+        pi = 3.14159;
+        area = pi * (Math.pow(raio,2));
+        System.out.println("O valor da area do circculo é:" + area);*/
+
+        /* 
+        //Exercicio 3
+        System.out.println("Iniciando programa para calcular a diferença do produto de duas duplas");
+        double n1, n2, n3, n4, resultado;
+        System.out.println("Digite o primeiro numero da primeira dupla:");
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextDouble();
+        System.out.println("Digite o segundo numero da primeira dupla:");
+        Scanner sc2 = new Scanner(System.in);
+        n2 = sc2.nextDouble();
+        System.out.println("Digite o primeiro numero da segunda dupla:");
+        Scanner sc3 = new Scanner(System.in);
+        n3 = sc3.nextDouble();
+        System.out.println("Digite o segundo numero da segunda dupla:");
+        Scanner sc4 = new Scanner(System.in);
+        n4 = sc4.nextDouble();
+        resultado = (n1 * n2) - (n3 * n4);
+        System.out.println("A diferença entre o produto das duas duplas é: " + resultado);*/
+
+        /* 
+        //Exercicio 4 
+        System.out.println("Iniciando programa para calcular o salario de um funcionario");
+        double numero, horas, valHora, salario;
+        System.out.println("Digite o numero do funcionario:");
+        Scanner sc = new Scanner(System.in);
+        numero = sc.nextDouble();
+        System.out.println("Digite o numero de horas trabalhadas:");
+        Scanner sc2 = new Scanner(System.in);
+        horas = sc2.nextDouble();
+        System.out.println("Digite o primeiro numero da segunda dupla:");
+        Scanner sc3 = new Scanner(System.in);
+        valHora = sc3.nextDouble();
+        salario = horas * valHora;
+        System.out.printf("O funcionario de numero %.0f tem um salario de R$ %.2f", numero, salario);*/
+
+        /* 
+        //Exercicio 5 
+
+        System.out.println("Iniciando programa para calcular o valor de uma compra de peças de roupa");
+        double cod1, qtd1, val1, cod2, qtd2, val2, valorCompra;
+        System.out.print("Digite o código da primeira roupa:");
+        Scanner sc = new Scanner(System.in);
+        cod1 = sc.nextDouble();
+        System.out.print(", agora a quantidade da primeira roupa:");
+        Scanner sc2 = new Scanner(System.in);
+        qtd1 = sc2.nextDouble();
+        System.out.print(", por fim o valor unitario:");
+        Scanner sc3 = new Scanner(System.in);
+        val1 = sc3.nextDouble();
+        System.out.print("Digite o código da segunda roupa:");
+        Scanner sc4 = new Scanner(System.in);
+        cod2 = sc4.nextDouble();
+        System.out.print(", agora a quantidade da segunda roupa:");
+        Scanner sc5 = new Scanner(System.in);
+        qtd2 = sc5.nextDouble();
+        System.out.print(", por fim o valor unitario:");
+        Scanner sc6 = new Scanner(System.in);
+        val2 = sc6.nextDouble();
+        valorCompra = (qtd1 * val1) + (qtd2 * val2);
+        System.out.println("O valor a pagar é: " + valorCompra);*/
+
+    }
+    
+}
